6d708b23c20d101147dc6da3641f7788c8258ca5
Mike van Engelen <mike@rocktheweb.nl>
2023-05-26 12:15:28 +0200

    Wecannowretrieveapiinfoyippie

--- fontys-frontend/src/App.js
+++ fontys-frontend/src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://groopyswoopyapiweb3.azurewebsites.net/api/User/1")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    console.log(data); // Log the data whenever it changes
+  }, [data]);
+
   return (
     <div className="App">
       <header className="App-header">
